356badd6b60882748abad650e06d20b66648e93e
Ian Gacek <gacek004@umn.edu>
2019-07-02 15:17:47 -0500

    Functioning "add stock" command

--- bamazonManager.js
+++ bamazonManager.js
@@ -54,13 +54,37 @@ var viewLowInventory = function () {
 };
 
 var addInventory = function () {
-    connection.query("SELECT * FROM products WHERE stock_quantity <= 50", function (err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
+      inquirer.prompt({
+        name: "choice",
+        type: "rawlist",
+        // This loop displays objects in an array
+        choices: function (value) { var choiceArray = []; for (var i = 0; i < res.length; i++) { choiceArray.push(res[i].product_name); } return choiceArray; }, message: "What item would you like to add stock to?"
+      }).then(function (answer) {
         for (var i = 0; i < res.length; i++) {
-            console.log(res);
+          if (res[i].product_name == answer.choice) {
+            var chosenItem = res[i];
+            inquirer.prompt({
+              name: "addQuantity",
+              type: "input",
+              message: "How much stock would you like to add?",
+              validate: function (value) {
+                if (isNaN(value) == false) {
+                  return true;
+                } else {
+                  return false;
+                }
+              },
+            }).then(function (answer) {
+                connection.query("UPDATE products SET stock_quantity = stock_quantity + " + answer.addQuantity + " WHERE item_id = " + chosenItem.item_id);
+                // INSERT LINE TO ADD UPDATED QUANTITY HERE
+                start();
+            })
+          }
         }
-        start();
+      })
     })
-};
+  }
 
 var addNewProduct = function () {
     connection.query("SELECT * FROM products WHERE stock_quantity <= 50", function (err, res) {
